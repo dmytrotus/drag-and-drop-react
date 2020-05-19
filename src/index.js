02556ff95ee1ce2bfec590ae99cd3470d9a8e665
Dmytro Tus <dmytrotus@gmail.com>
2020-05-19 23:54:45 +0200

    reorder a list

--- src/index.js
+++ src/index.js
@@ -1,6 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import '@atlaskit/css-reset';
+import { DragDropContext } from 'react-beautiful-dnd';
+import initialData from './initial-data';
+import Column from './column';
 
-const App = () => 'Hello world';
+class App extends React.Component{
+	state = initialData;
+
+	onDragEnd = result => {
+		//
+	}
+
+	render(){
+		return (
+			<DragDropContext
+			onDragEnd={this.onDragEnd}
+
+			>
+		{this.state.columnOrder.map((columnId) => {
+			const column = this.state.columns[columnId];
+			const tasks = column.taskIds.map(taskId => this.state.tasks[taskId]);
+
+			return <Column key={column.id} column={column} tasks={tasks} />
+		})}
+			</DragDropContext>
+		);
+	}
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
